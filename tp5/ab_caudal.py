df8c6529f90fbe06a0707dd800460dcc454a33d5
sebas favaron <sfavaron@balanz.com>
2021-06-05 10:22:46 -0300

    MOD simplified ab_caudal

--- tp5/ab_caudal.py
+++ tp5/ab_caudal.py
@@ -22,26 +22,13 @@ ax1.set_ylabel('Particulas que escaparon', fontsize=27)
 ax1.tick_params(axis='both', which='major', labelsize=20, width=2.5, length=10)
 
 
-# Set up data - find average amounts for each timestep (very inefficient, I don't care)
-granularity = 50  # 50 points per second
-times = [i/granularity for i in range(1000 * granularity)]
-amounts_avg = []
-for time in times:
-    amounts_for_time = []
-    for _times in list_of_times:
-        for amount, _time in enumerate(_times):
-            if _time > time:
-                amounts_for_time.append(amount)  # o amount-1
-                break
-    if len(amounts_for_time) != 0:
-        amounts_avg.append(avg(amounts_for_time))
-    else:
-        times = times[:len(amounts_avg)]
-        break
+# Set up data
+x = [avg(times) for times in zip(*list_of_times)]
+y = [i for i in range(200)]
 
-x2 = int(0.7 * len(amounts_avg))
-x1 = int(0.2 * len(amounts_avg))
-print('Caudal (pendiente estable):', (amounts_avg[x2] - amounts_avg[x1]) / (x2 - x1) * granularity)
+i1 = int(len(x) * 0.4)
+i2 = int(len(x) * 0.6)
+print('Caudal (pendiente estable):', (y[i2] - y[i1]) / (x[i2] - x[i1]))
 
 # interval = 1
 # caudal = [(amounts_avg[x] - amounts_avg[x - interval]) / (interval) * granularity for x in range(interval, len(amounts_avg))]
@@ -49,12 +36,12 @@ print('Caudal (pendiente estable):', (amounts_avg[x2] - amounts_avg[x1]) / (x2 -
 # caudal = caudal[:int(len(amounts_avg)*0.9)]
 
 # Plot
-ax1.errorbar(times, amounts_avg)
-# ax1.errorbar(times, caudal)
+ax1.errorbar(x, y, alpha=0.4)
+ax1.plot([x[i1], x[i2]], [y[i1], y[i2]], color="black")
 fig1=plt.gcf()
 
 # Descomentar lo de abajo para ver la pendiente
-# if savefile_name != '':
-#     plt.savefig(savefile_name)
-# else:
-#     plt.show()
+if savefile_name != '':
+    plt.savefig(savefile_name)
+else:
+    plt.show()
